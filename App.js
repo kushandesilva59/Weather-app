89b8924eab5ecbfd6a6b296e7e01c837c0931ecf
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-18 13:23:47 +0530

    init project

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textSize}>Nalini de silva!</Text>
+      <Text style={styles.textSize}>Namal de silva!</Text>
+      <Text style={styles.textSize}>Jayan de silva!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textSize:{
+    fontSize: 28
+  }
+});
